1c28f2c9276c91a40a0a3c0706fce1ccef8b251c
Frontend Developer <90094867+yusufie@users.noreply.github.com>
2024-03-03 20:01:59 +0300

    feature: Filterbox and RoomCard components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout/Layout";
 import Carousel from "@/components/Carousel/Carousel";
 import PageSearch from "@/components/PageSearch/PageSearch";
+import Filterbox from "@/components/Filterbox/Filterbox";
 import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <Layout>
       <Carousel />
         <PageSearch />
+        <Filterbox />
       <Footer />
     </Layout>
   );
--- components/Filterbox/Filterbox.tsx
+++ components/Filterbox/Filterbox.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import RoomCard from "@/components/RoomCard/RoomCard";
+import { Room } from "@/models/room";
+import rooms from "@/lib/rooms.json";
 
 const Filterbox: React.FC = () => {
   return (
-    <section>
-        Filterbox
+    <section className="flex mt-20 justify-between flex-wrap">
+      {rooms.map((room: any) => (
+        <RoomCard key={room._id} room={room} />
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default Filterbox
+export default Filterbox;
