1a26c3ddc0030dd92308408152ec7d2ee78d7b22
Frontend Developer <90094867+yusufie@users.noreply.github.com>
2024-03-04 22:56:10 +0300

    Fix null reference exception in HotelPhotoGallery

--- app/rooms/[slug]/page.tsx
+++ app/rooms/[slug]/page.tsx
@@ -19,7 +19,7 @@ const RoomDetails = () => {
 
   return (
      <Layout>
-      <HotelPhotoGallery photos={room?.images} />
+      <HotelPhotoGallery photos={room?.images || []} />
 
       <div className='container mx-auto mt-20'>
         <div className='md:grid md:grid-cols-12 gap-10 px-3'>
